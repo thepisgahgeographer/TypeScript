1a69edb38385e29c2a19e8a5e3f31e377203c32a
Yuri Potawsky <potawsky.yuri@gmail.com>
2021-01-13 16:06:12 -0500

    1.13

--- stringNumber.js
+++ stringNumber.js
@@ -1,3 +1,6 @@
+var c = 5;
+var d = "6";
+//console.log(a + b);
 var a = 5;
-var b = "6";
+var b = 6;
 console.log(a + b);
--- stringNumber.ts
+++ stringNumber.ts
@@ -1,5 +1,10 @@
-let a = 5;
+let c = 5;
 
-let b = "6";
+let d = "6";
 
-console.log(a + b);
+//console.log(a + b);
+
+let a: number = 5;
+let b: number = 6;
+
+console.log(a+b);
