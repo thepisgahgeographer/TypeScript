ecbbbae0c96d74071186e98074e920a54fde3da4
Yuri Potawsky <potawsky.yuri@gmail.com>
2021-01-13 21:39:30 -0500

    Interface

---
+++ test-age.js
@@ -0,0 +1,13 @@
+var canDrive = function (usr) {
+    console.log("user is", usr.name);
+    if (usr.age >= 16) {
+        console.log("allow to drive");
+    }
+    else {
+        console.log("do not allow to drive");
+    }
+};
+var tom = {
+    name: "tom"
+};
+canDrive(tom);
---
+++ test-age.ts
@@ -0,0 +1,19 @@
+let canDrive = (usr)=> {
+    console.log("user is", usr.name);
+
+    if (usr.age >= 16) {
+        console.log("allow to drive");
+    } else {
+        console.log("do not allow to drive");
+    }
+
+}
+
+
+const tom = {
+    name: "tom"
+}
+
+canDrive(tom); 
+
+
