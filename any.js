e3fff571ed750c438ac52f7953e5f35babea89b5
Yuri Potawsky <potawsky.yuri@gmail.com>
2021-01-15 16:10:55 -0500

    1.15

---
+++ any.js
@@ -0,0 +1,5 @@
+var val = 22;
+val = "String";
+val = new Array();
+val.push(33);
+console.log(val);
---
+++ any.ts
@@ -0,0 +1,8 @@
+let val: any = 22;
+
+val = "String";
+
+val = new Array();
+
+val.push(33);
+console.log(val);
